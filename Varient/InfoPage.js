157319ed2afdc4a8f8824b4e0832494f981ce2a2
Shukui Chen <shukui.chen@mail.utoronto.ca>
2020-10-15 17:08:41 -0400

    Added basic items to Info page. Added native-base to project dependency.

--- Varient/InfoPage.js
+++ Varient/InfoPage.js
@@ -1,20 +1,40 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Container, Content, List, ListItem, Separator, Text, View } from "native-base";
 
 class InfoPage extends React.Component {
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}
-            >
-                <Text>Information page</Text>
-            </View>
+            <Container>
+                <Content>
+                    <List>
+                        <Separator bordered>
+                            <Text>About</Text>
+                        </Separator>
+                        <ListItem onPress={this._onPressListItem}>
+                            <Text>Terms and Conditions</Text>
+                        </ListItem>
+                        <ListItem onPress={this._onPressListItem}>
+                            <Text>Privacy Policy</Text>
+                        </ListItem>
+                        <Separator bordered>
+                            <Text>Support</Text>
+                        </Separator>
+                        <ListItem onPress={this._onPressListItem}>
+                            <Text>Contact Varient</Text>
+                        </ListItem>
+                    </List>
+                    <View style={{ marginTop: 10 }}>
+                        <Text style={{ alignSelf: "center" }}>Version 0.1.0</Text>
+                        <Text style={{ alignSelf: "center" }}>Copyright Info</Text>
+                    </View>
+                </Content>
+            </Container>
         );
+    }
+
+    _onPressListItem() {
+
     }
 }
 
-export default InfoPage; 
+export default InfoPage;
