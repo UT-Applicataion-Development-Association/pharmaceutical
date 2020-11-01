69c2262be793ec7677370c8282fb7b8ea7e31457
JasmineSun33 <jasmine.jysun@gmail.com>
2020-11-01 01:07:12 -0500

    handled user input

--- Varient/LoginPage.js
+++ Varient/LoginPage.js
@@ -6,36 +6,79 @@ class LoginPage extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        username:null,
-        password:null,
+        username:"",
+        password:"",
+        userInputError: false,         // both username pwd required
+        errorMessage: "",
+      };
+    }
+
+    _inputCheck = () => {
+
+      if (this.state.username == "" || this.state.password == ""  ) {
+        this.setState({ userInputError: true });
+        this.setState({  errorMessage: "Please enter all the required field" });
+      } else {
+        this.setState({ userInputError: false });
+      }  
+    };
+
+    _handleLogin = async () => {
+      await this._inputCheck();
+      if ( !this.state.userInputError) {
+        const { username, password } = this.state;
+        console.log("User [", username,"] attempted to log in")
+
+        // TODO: check authentication credential
+        if(true){
+
+          this.setState({
+            userInputError: false,
+            errorMessage: "",
+            username:"",
+            password:""
+          });
+          this.props.navigation.navigate("Landing");
+
+        } else{
+          // TODO: catch other errors and return approperiate error message.
+          this.setState({  errorMessage: "User doesn't exist or password incorrect. " });
+        }
+
       };
     }
 
 
-
     render(){
         return (
-            <View style={styles.screen}>
-            <Text style={styles.greeting}>Welcome to Varient</Text>
+          <View style={styles.screen}>
+            <View style={styles.greetingView}>
+              <Text style={styles.greetingText}>Welcome to Varient</Text>
+              <Text style={styles.errorMessage}>
+                  {this.state.errorMessage}
+              </Text>
+
+            </View>
             <TextInput style={styles.textInput}
               placeholder="Username"
               value={this.state.username}
               onChangeText={text => this.setState({username:text})}
             />
             <TextInput style={styles.textInput}
+              secureTextEntry
               placeholder="Password"
               value={this.state.password}
               onChangeText={text => this.setState({password:text})}
 
             />
-            <Button title="Sign in!" onPress={() => this.props.navigation.navigate("Landing")} />
+            <Button title="Sign in!" onPress={() => this._handleLogin()} />
             <Button 
               style={{fontSize:13}} 
               color="grey" 
               title="Not a user? Sign up!" 
               onPress={() => this.props.navigation.navigate("Signup")} />
 
-            
+      
           </View>
         );
     }
@@ -47,9 +90,21 @@ const styles = StyleSheet.create({
 
   },
 
-  greeting:{
-    marginVertical:100,
+  greetingText:{
     fontSize:18,
+  },
+
+  errorMessage: {
+    color: "red",
+    fontWeight:"600",
+    fontSize:16
+  },
+
+  greetingView:{
+    marginTop: 100,
+    marginBottom:50,
+    alignItems:"center",
+    
   },
 
   textInput :{
